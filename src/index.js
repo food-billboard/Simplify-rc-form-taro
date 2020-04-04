59825c53674f7b0d3caaea517f2053bac77282a9
gelingfeng <18368003190@163.com>
2020-04-04 18:29:09 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import Taro, { Component } from '@tarojs/taro'
+import { Block } from '@tarojs/components'
+import { createFieldsStore, getAllFieldsStoreName, deleteFieldsStore } from './fieldsStore'
+
+export default class extends Component {
+
+  constructor() {
+    super(...arguments)
+    this.init()
+  }
+
+  componentWillReceiveProps(props) {
+    const { options={} } = this.fieldsStore
+    if(options.mapPropsToFields) {
+      this.fieldsStore.setFields(options.mapPropsToFields(props))
+    }
+  }
+
+  init() {
+    if(!this.props.name) return
+    this.fieldsStore = createFieldsStore(this.props.name)
+  }
+
+  render() {
+
+    return (
+      <Block>
+        {
+          this.props.renderForm
+        }
+      </Block>
+    )
+  }
+
+}
+
+export {
+  createFieldsStore, 
+  getAllFieldsStoreName, 
+  deleteFieldsStore
+}
